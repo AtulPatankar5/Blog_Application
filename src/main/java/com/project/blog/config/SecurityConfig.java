394f6e0a852420a3ba0eb4b08c8eb2a86d1a9d94
AtulPatankar5 <atulsantoshpatankar5@gmail.com>
2023-09-29 19:03:42 +0530

    Backend Finish

--- src/main/java/com/project/blog/config/SecurityConfig.java
+++ src/main/java/com/project/blog/config/SecurityConfig.java
@@ -4,20 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.project.blog.security.CustomUserDetailService;
 import com.project.blog.security.JwtAuthenticationEntryPoint;
 import com.project.blog.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+	public static final String[] PUBLIC_URL = { "/api/v1/auth/**", "/v3/api-docs",
+			"/v2/api-docs","/swagger-resources/**","/swagger-ui/**","/webjars/**"	};
 
 	@Autowired
 	private JwtAuthenticationEntryPoint point;
@@ -25,20 +36,40 @@ public class SecurityConfig {
 	@Autowired
 	private JwtAuthenticationFilter filter;
 
+	@Autowired
+	private CustomUserDetailService customerUserService;
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 // auth.requestMatchers("/home/**").authenticated()
 		// configuration
 		http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
-				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().
-						requestMatchers(HttpMethod.DELETE,"/api/users/**").hasRole("ADMIN").
-						requestMatchers(HttpMethod.GET).permitAll().// all GET method will be authorized 
+				.authorizeHttpRequests(auth -> auth.requestMatchers(PUBLIC_URL).permitAll()// permit all swagger and authentication URL								// link
+						.requestMatchers("/api/users/**").permitAll()//.hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET).permitAll().// all GET method will be authorized
 						anyRequest().authenticated())
 				.exceptionHandling(ex -> ex.authenticationEntryPoint(point))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+		http.authenticationProvider(daoAuthenticationProvider());
 		return http.build();
+	}
+	
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+		provider.setUserDetailsService(customerUserService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+		}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+		return builder.getAuthenticationManager();
 	}
 
 }
--- src/main/java/com/project/blog/security/CustomUserDetailService.java
+++ src/main/java/com/project/blog/security/CustomUserDetailService.java
@@ -1,7 +1,6 @@
 package com.project.blog.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
